7e1a009795662c89f27817353298e2a29bc47314
Joshua Maxcy <kanemaxcy@gmail.com>
2019-12-26 20:34:04 -0500

    splitting app out

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const admin = require('./routes/admin');
+const shop = require('./routes/shop');
 
 
 const app = express();
+const api = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/v1', api);
+api.use('/shop', shop);
+api.use('/admin', admin);
 
 app.use((req, res, next) => {
-  res.send('Hello from expressjs!');
+  res.send('<h1>404 Page not found</h1>')
 });
-
 const server = http.createServer(app);
 server.listen(3000);
