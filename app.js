91c204fdc7c5efce4e78f00c960f0dea5785cf86
Joshua Maxcy <kanemaxcy@gmail.com>
2019-12-28 00:17:16 -0500

    serving html files from express

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const shop = require('./routes/shop');
 
 
 const app = express();
-const api = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/v1', api);
-api.use('/shop', shop);
-api.use('/admin', admin);
+
+app.use('/shop', shop);
+app.use('/admin', admin);
 
 app.use((req, res, next) => {
   res.send('<h1>404 Page not found</h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-  res.sendStatus(404);
+  const page = path.join(__dirname, '../', 'views', 'add-product.html');
+  res.sendFile(page);
 });
 
 module.exports = router;
